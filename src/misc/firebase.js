7845828431edd2bfcba3853ada7111effcd3c5ef
AndSim <siminiucandrew@gmail.com>
2020-11-28 06:40:45 +0300

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyCJJRLGMemNif3wG7hFc05LEghS9c0kwZs",
+    authDomain: "reactjs-firebase-chatapp-1.firebaseapp.com",
+    databaseURL: "https://reactjs-firebase-chatapp-1.firebaseio.com",
+    projectId: "reactjs-firebase-chatapp-1",
+    storageBucket: "reactjs-firebase-chatapp-1.appspot.com",
+    messagingSenderId: "95612104118",
+    appId: "1:95612104118:web:d9b91d01dd257f3d9a2517"
+};
+
+const app = firebase.initializeApp(config);
